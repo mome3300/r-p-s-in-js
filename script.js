a5d5f4d6a4c8bff5285591e7ec77c99131d708be
mome3300 <152378173+mome3300@users.noreply.github.com>
2025-10-20 18:13:15 +0200

    Update plaRound logic.

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function getHumanChoice() {
 
     // logic for human selection.
 
-    const choice = prompt();
+    const choice = prompt("Enter 'Rock', 'Paper' or 'Scissors' in the box.");
 
     selection = ["rock", "paper", "scissors"];
 
@@ -32,21 +32,27 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
 
-    if (humanChoice == "rock", computerChoice == "scissors") {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
 
-    } else if (humanChoice == "paper", computerChoice == "rock") {
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
 
-    } else if (humanChoice == "scissors", computerChoice == "paper") {
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
 
-    } else if (humanChoice == computerChoice) {
+    } else if (humanChoice === computerChoice) {
         console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. It's a tie.`);
 
+    } else if (humanChoice === undefined) {
+        console.log(`Please reload this page.`);
+
+    } else if (humanChoice !== computerChoice) {
+        console.log(`Please make a proper selection!`);
+
     } else {
-        console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You lose, ${humanChoice} beats ${computerChoice}.`);
-    }
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You lose, ${humanChoice} beats ${computerChoice}.`);
+    };
 };
 
 const humanSelection = getHumanChoice();

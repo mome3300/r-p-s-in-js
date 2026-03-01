eef65268324ac3c21b64ddf24e80f24740222341
mome3300 <152378173+mome3300@users.noreply.github.com>
2026-03-01 16:22:03 +0200

    Refactor getHumanChoice() logic. Remove redundant code

--- script.js
+++ script.js
@@ -13,69 +13,48 @@ function getComputerChoice() {
     };
 };
 
+const choice = prompt("Enter 'Rock', 'Paper' or 'Scissors' in the box.");
+
 function getHumanChoice() {
 
     // human selection.
 
-    choice = prompt("Enter 'Rock', 'Paper' or 'Scissors' in the box.");
+    let humanChoice = String(choice);
     
-    let humanChoice = String(choice).toLowerCase();
-     
+    return humanChoice.toLowerCase()
 
-    if (humanChoice === "rock") {
-
-        return "rock";
-
-    } else if (humanChoice === "paper") {
-
-        return "paper";
- 
-    } else if (humanChoice === "scissors") {
-
-        return "scissors";
-
-    }
-    
 };
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
 function playGame() {
 
-
     let humanScore = 0;
     let computerScore = 0;
-    
+
     function playRound(humanChoice, computerChoice) {
 
         let reload = "Please reload this page";
 
         if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
+            console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
 
             humanScore++
-            console.log(reload)
-
+            
         } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
+            console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
 
             humanScore++
-            console.log(reload)
 
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
+            console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
 
             humanScore++
-            console.log(reload)
 
         } else if (humanChoice === computerChoice) {
-            console.log( `You chose ${humanChoice}, computer chose ${computerChoice}. It's a tie.`);
+            console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. It's a tie.`);
 
             humanScore++
             computerScore++
-            console.log(reload)
-
+            
         } else if (humanChoice == undefined) {
             console.log(`Please reload this page and make a proper selection!`);
 
@@ -83,14 +62,18 @@ function playGame() {
             console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You lose, ${computerChoice} beats ${humanChoice}.`);
 
             computerScore++
-            console.log(reload);
         };
 
         let score = `H${humanScore} : C${computerScore}`;
-        console.log(score)
+
+        console.log(score);
     }
+
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
     playRound(humanSelection, computerSelection);
 }
 
-playGame()
+playGame();
